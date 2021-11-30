b7bf4c21b42ed17a1a38ed54d269b9dec3600e90
HiImDanix <danixkanepe@gmail.com>
2021-11-30 15:15:26 +0100

    Unnecessary code removal

--- src/view/App.java
+++ src/view/App.java
@@ -1,6 +1,6 @@
 package view;
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         MenuMain menuMain = MenuMain.getInstance();
         menuMain.show();
     }
