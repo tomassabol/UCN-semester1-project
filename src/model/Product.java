cbe09f799a7aa74268ead39e2ce2094effad4466
Tomáš Sabol <tomassabol2@gmail.com>
2021-11-30 11:30:20 +0100

    created product constructor, varoiables, set & get methods

--- src/model/Product.java
+++ src/model/Product.java
@@ -8,12 +8,65 @@ package model;
  *
  */
 public class Product {
-
+	private int productID;
+	private String name;
+	private String description;
+	private int minStock;
+	private int maxStock; 
 	/**
 	 * 
 	 */
-	public Product() {
-		// TODO Auto-generated constructor stub
+	public Product(int productID, String name, String description, int minStock, int maxStock) {
+		this.productID = productID;
+		this.name = name;
+		this.description = description;
+		this.minStock = minStock;
+		this.maxStock = maxStock;
+	}
+
+	// set/get productID
+	public int getProductID() {
+		return this.productID;
+	}
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+	// set/get name
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// set/get description
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// set/get minStock
+	public int getMinStock() {
+		return this.minStock;
+	}
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+
+	// set/get maxStock
+	public int getMaxStock() {
+		return this.maxStock;
+	}
+
+	public void setMaxStock(int maxStock) {
+		this.maxStock = maxStock;
 	}
 
 }
