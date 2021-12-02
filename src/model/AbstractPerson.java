681c59c0ac20ae5b08cd4baf2254d2fd350e4a1f
HiImDanix <danixkanepe@gmail.com>
2021-12-02 13:23:47 +0100

    Added ID to Abstract Person

--- src/model/AbstractPerson.java
+++ src/model/AbstractPerson.java
@@ -5,19 +5,24 @@ import java.time.LocalDateTime;
 
 public abstract class AbstractPerson {
 	/**
-	 * Fields for AbstractPerson abstract class
+	 * Fields for AbstractPerson class
 	 */
+	public int ID;
 	private String firstName;
 	private String lastName;
 	private String address;
-	private String mobil;
+	private String mobile;
 	private LocalDateTime birthDate;
 
-	public AbstractPerson(String firstName, String lastName, String address, String mobil, LocalDateTime birthDate){
+	/*
+	 * Constructor
+	 */
+	protected AbstractPerson(int ID, String firstName, String lastName, String address, String mobile, LocalDateTime birthDate){
+		this.ID = ID;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
-		this.mobil = mobil;
+		this.mobile = mobile;
 		this.birthDate = birthDate;
 	}
 
