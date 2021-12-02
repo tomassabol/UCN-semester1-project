75551aca497336b1496ec797610aa922eb76e621
HiImDanix <danixkanepe@gmail.com>
2021-12-02 13:25:30 +0100

    Getters & setters for abstract person

--- src/model/AbstractPerson.java
+++ src/model/AbstractPerson.java
@@ -26,4 +26,46 @@ public abstract class AbstractPerson {
 		this.birthDate = birthDate;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public LocalDateTime getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDateTime birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	
+
 }
