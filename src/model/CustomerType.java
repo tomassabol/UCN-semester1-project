721101724b6b4b4bdf5ee0166d3cd957be318478
HiImDanix <danixkanepe@gmail.com>
2021-12-08 20:55:10 +0100

    Implemented customer type

--- src/model/CustomerType.java
+++ src/model/CustomerType.java
@@ -2,17 +2,33 @@ package model;
 
 public class CustomerType {
 	
-	private String type;
+	private String name;
+	private int discountPercentage;
 
-	public CustomerType(String type) {
-		this.type = type;
+	public CustomerType(String type, int discountPercentage) {
+		if (discountPercentage < 0 || discountPercentage > 100) {
+			throw new IllegalArgumentException("Customer's discount percentage must be between 0 and 100");
+		}
+		this.name = type;
+		this.discountPercentage = discountPercentage;
 		
 		
 	}
-	
-	@Override
-	public String toString() {
-		return this.type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDiscountPercentage() {
+		return discountPercentage;
+	}
+
+	public void setDiscountPercentage(int discountPercentage) {
+		this.discountPercentage = discountPercentage;
 	}
 	
 	
