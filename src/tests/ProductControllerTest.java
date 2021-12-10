db4d11a3fb6b559ae5b31422d29f7709a939b304
attilaUCN <10407547@ucn.dk>
2021-12-10 10:54:13 +0100

    productControllerTestDone
    productControllerTestDone

--- src/tests/ProductControllerTest.java
+++ src/tests/ProductControllerTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +24,7 @@ class ProductControllerTest {
 
 	private ProductContainer productCon;
 	private Product product1;
+	private Product product2;
 
 	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
@@ -36,6 +39,7 @@ class ProductControllerTest {
 		productCon = ProductContainer.getInstance();
 		
 		product1 = new Product(12,"product1","description", 30, 40,LocalDateTime.now());
+		product2 = new Product(12,"product2","description2", 20, 70,LocalDateTime.now());
 		
 		
 		
@@ -65,15 +69,20 @@ class ProductControllerTest {
 	@Test
 	public void testCreateLoaningPrice() {
 		
-		LoaningPrice loaningPrice = new LoaningPrice(BigDecimal.valueOf(20), LocalDateTime.now());
+		LoaningPrice loaningPrice = new LoaningPrice(BigDecimal.valueOf(6500), LocalDateTime.now());
 		product1.addLoaningPrice(loaningPrice);
-		
 		assertNotNull(loaningPrice);
 		
 	}
-	
+	@Test
+	public void testLoanPriceSholdNotBeLessThanZero() {
+		IllegalArgumentException thrown = 	assertThrows(IllegalArgumentException.class, () -> {
+			LoaningPrice loaningPrice = new LoaningPrice(BigDecimal.valueOf(-10), LocalDateTime.now());
+			product2.addLoaningPrice(loaningPrice);
+		});
+		assertEquals("Price per hour shouldn't be below 0", thrown.getMessage());
 		
-		
+	}	
 			
 }
 
