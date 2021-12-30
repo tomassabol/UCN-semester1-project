1aa164cf4de1cef049d338ba2b8a788106fef313
HiImDanix <danixkanepe@gmail.com>
2021-12-31 00:33:19 +0100

    Refactoring & comments

--- src/gui/ShoppingCart.java
+++ src/gui/ShoppingCart.java
@@ -71,6 +71,7 @@ public class ShoppingCart extends JDialog {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		// ***** TOP PANEL *****
 		JPanel topPanel = new JPanel();
 		getContentPane().add(topPanel, BorderLayout.NORTH);
 		GridBagLayout gbl_topPanel = new GridBagLayout();
@@ -79,149 +80,166 @@ public class ShoppingCart extends JDialog {
 		gbl_topPanel.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_topPanel.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
 		topPanel.setLayout(gbl_topPanel);
-		
-		JLabel lblTitle = new JLabel(
-				String.format("%s %s's shopping cart", 
-						customer.getFirstName(), 
-						customer.getLastName()
-				)
-		);
-		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
-		gbc_lblTitle.gridwidth = 3;
-		gbc_lblTitle.insets = new Insets(0, 0, 5, 0);
-		gbc_lblTitle.gridx = 0;
-		gbc_lblTitle.gridy = 0;
-		topPanel.add(lblTitle, gbc_lblTitle);
-		
-		btnClear = new JButton("Clear");
-		GridBagConstraints gbc_btnTest_2 = new GridBagConstraints();
-		gbc_btnTest_2.insets = new Insets(0, 0, 5, 5);
-		gbc_btnTest_2.gridx = 1;
-		gbc_btnTest_2.gridy = 1;
-		topPanel.add(btnClear, gbc_btnTest_2);
-		
-		btnAddItem = new JButton("Add item");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
-		gbc_btnNewButton.gridx = 2;
-		gbc_btnNewButton.gridy = 1;
-		topPanel.add(btnAddItem, gbc_btnNewButton);
-		
+			// ***** Title *****
+			JLabel lblTitle = new JLabel(
+					String.format("%s %s's shopping cart", 
+							customer.getFirstName(), 
+							customer.getLastName()
+					)
+			);
+			GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+			gbc_lblTitle.gridwidth = 3;
+			gbc_lblTitle.insets = new Insets(0, 0, 5, 0);
+			gbc_lblTitle.gridx = 0;
+			gbc_lblTitle.gridy = 0;
+			topPanel.add(lblTitle, gbc_lblTitle);
+			
+			// ***** Button: add item *****
+			btnClear = new JButton("Clear");
+			GridBagConstraints gbc_btnTest_2 = new GridBagConstraints();
+			gbc_btnTest_2.insets = new Insets(0, 0, 5, 5);
+			gbc_btnTest_2.gridx = 1;
+			gbc_btnTest_2.gridy = 1;
+			topPanel.add(btnClear, gbc_btnTest_2);
+			
+			// ***** button: clear  *****
+			btnAddItem = new JButton("Add item");
+			GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+			gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+			gbc_btnNewButton.gridx = 2;
+			gbc_btnNewButton.gridy = 1;
+			topPanel.add(btnAddItem, gbc_btnNewButton);
+			
+		// ***** Middle panel *****
 		JPanel middlePanel = new JPanel();
 		getContentPane().add(middlePanel, BorderLayout.CENTER);
 		middlePanel.setLayout(new BorderLayout(0, 0));
+			
+			// ***** Scroll panel *****
+			JScrollPane scrollPanel = new JScrollPane();
+			middlePanel.add(scrollPanel);
 		
-		JScrollPane scrollPanel = new JScrollPane();
-		middlePanel.add(scrollPanel);
+				// ***** Table *****
+				tableModel = new ShoppingCartModel(this.customer.getShoppingCart());
+				mainTable = new JTable();
+				mainTable.setModel(tableModel);
+				mainTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				scrollPanel.setViewportView(mainTable);
 		
-		// Table
-		tableModel = new ShoppingCartModel(this.customer.getShoppingCart());
-		mainTable = new JTable();
-		mainTable.setModel(tableModel);
-		mainTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		scrollPanel.setViewportView(mainTable);
-		
+		// ***** Bottom panel *****
 		JPanel bottomPanel = new JPanel();
 		contentPane.add(bottomPanel, BorderLayout.SOUTH);
 		bottomPanel.setLayout(new BorderLayout(0, 0));
 		
-		JPanel tableBottomOptionsPanel = new JPanel();
-		bottomPanel.add(tableBottomOptionsPanel, BorderLayout.NORTH);
-		GridBagLayout gbl_tableBottomOptionsPanel = new GridBagLayout();
-		gbl_tableBottomOptionsPanel.columnWidths = new int[]{0, 0, 0, 0, 0};
-		gbl_tableBottomOptionsPanel.rowHeights = new int[]{0, 0};
-		gbl_tableBottomOptionsPanel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_tableBottomOptionsPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		tableBottomOptionsPanel.setLayout(gbl_tableBottomOptionsPanel);
+			// ***** Panel for table options at the bottom *****
+			JPanel tableBottomOptionsPanel = new JPanel();
+			bottomPanel.add(tableBottomOptionsPanel, BorderLayout.NORTH);
+			GridBagLayout gbl_tableBottomOptionsPanel = new GridBagLayout();
+			gbl_tableBottomOptionsPanel.columnWidths = new int[]{0, 0, 0, 0, 0};
+			gbl_tableBottomOptionsPanel.rowHeights = new int[]{0, 0};
+			gbl_tableBottomOptionsPanel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_tableBottomOptionsPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+			tableBottomOptionsPanel.setLayout(gbl_tableBottomOptionsPanel);
 		
-		btnView = new JLink("View", JLink.COLORS.GREEN);
-		btnView.setEnabled(false);
-		GridBagConstraints gbc_btnView = new GridBagConstraints();
-		gbc_btnView.insets = new Insets(0, 0, 0, 5);
-		gbc_btnView.gridx = 1;
-		gbc_btnView.gridy = 0;
-		tableBottomOptionsPanel.add(btnView, gbc_btnView);
+				// ***** Button: View *****
+				btnView = new JLink("View", JLink.COLORS.GREEN);
+				btnView.setEnabled(false);
+				GridBagConstraints gbc_btnView = new GridBagConstraints();
+				gbc_btnView.insets = new Insets(0, 0, 0, 5);
+				gbc_btnView.gridx = 1;
+				gbc_btnView.gridy = 0;
+				tableBottomOptionsPanel.add(btnView, gbc_btnView);
+			
+				// ***** Button: edit quantity *****
+				btnEditQuantity = new JLink("Edit Quantity", JLink.COLORS.INDIGO);
+				btnEditQuantity.setEnabled(false);
+				GridBagConstraints gbc_btnEditQuantity = new GridBagConstraints();
+				gbc_btnEditQuantity.insets = new Insets(0, 0, 0, 5);
+				gbc_btnEditQuantity.gridx = 2;
+				gbc_btnEditQuantity.gridy = 0;
+				tableBottomOptionsPanel.add(btnEditQuantity, gbc_btnEditQuantity);
+			
+				// ***** Button: Remove *****
+				btnRemove = new JLink("Remove", JLink.COLORS.RED);
+				btnRemove.setEnabled(false);
+				GridBagConstraints gbc_btnRemove = new GridBagConstraints();
+				gbc_btnRemove.gridx = 3;
+				gbc_btnRemove.gridy = 0;
+				tableBottomOptionsPanel.add(btnRemove, gbc_btnRemove);
 		
-		btnEditQuantity = new JLink("Edit Quantity", JLink.COLORS.INDIGO);
-		btnEditQuantity.setEnabled(false);
-		GridBagConstraints gbc_btnEditQuantity = new GridBagConstraints();
-		gbc_btnEditQuantity.insets = new Insets(0, 0, 0, 5);
-		gbc_btnEditQuantity.gridx = 2;
-		gbc_btnEditQuantity.gridy = 0;
-		tableBottomOptionsPanel.add(btnEditQuantity, gbc_btnEditQuantity);
+			// ***** Panel for pricing and create quote button *****
+			JPanel priceAndSubmitPanel = new JPanel();
+			bottomPanel.add(priceAndSubmitPanel, BorderLayout.SOUTH);
+			GridBagLayout gbl_priceAndSubmitPanel = new GridBagLayout();
+			gbl_priceAndSubmitPanel.columnWidths = new int[]{0, 17, 0, 0, 0};
+			gbl_priceAndSubmitPanel.rowHeights = new int[]{0, 0, 0, 0, 0};
+			gbl_priceAndSubmitPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+			gbl_priceAndSubmitPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			priceAndSubmitPanel.setLayout(gbl_priceAndSubmitPanel);
 		
-		btnRemove = new JLink("Remove", JLink.COLORS.RED);
-		btnRemove.setEnabled(false);
-		GridBagConstraints gbc_btnRemove = new GridBagConstraints();
-		gbc_btnRemove.gridx = 3;
-		gbc_btnRemove.gridy = 0;
-		tableBottomOptionsPanel.add(btnRemove, gbc_btnRemove);
-		
-		JPanel priceAndSubmitPanel = new JPanel();
-		bottomPanel.add(priceAndSubmitPanel, BorderLayout.SOUTH);
-		GridBagLayout gbl_priceAndSubmitPanel = new GridBagLayout();
-		gbl_priceAndSubmitPanel.columnWidths = new int[]{0, 17, 0, 0, 0};
-		gbl_priceAndSubmitPanel.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gbl_priceAndSubmitPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_priceAndSubmitPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		priceAndSubmitPanel.setLayout(gbl_priceAndSubmitPanel);
-		
-		JLabel lblSubtotal = new JLabel("Subtotal:");
-		GridBagConstraints gbc_lblSubtotal = new GridBagConstraints();
-		gbc_lblSubtotal.anchor = GridBagConstraints.WEST;
-		gbc_lblSubtotal.insets = new Insets(0, 0, 5, 5);
-		gbc_lblSubtotal.gridx = 0;
-		gbc_lblSubtotal.gridy = 0;
-		priceAndSubmitPanel.add(lblSubtotal, gbc_lblSubtotal);
-		
-		lblSubtotalValue = new JLabel("N/A");
-		lblSubtotalValue.setForeground(new Color(102, 102, 102));
-		GridBagConstraints gbc_lblSubtotalValue = new GridBagConstraints();
-		gbc_lblSubtotalValue.anchor = GridBagConstraints.WEST;
-		gbc_lblSubtotalValue.insets = new Insets(0, 0, 5, 5);
-		gbc_lblSubtotalValue.gridx = 2;
-		gbc_lblSubtotalValue.gridy = 0;
-		priceAndSubmitPanel.add(lblSubtotalValue, gbc_lblSubtotalValue);
-		
-		JLabel lblDiscount = new JLabel(customer.getCustomerType().getName() + " Discount:");
-		GridBagConstraints gbc_lblDiscount = new GridBagConstraints();
-		gbc_lblDiscount.anchor = GridBagConstraints.WEST;
-		gbc_lblDiscount.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDiscount.gridx = 0;
-		gbc_lblDiscount.gridy = 1;
-		priceAndSubmitPanel.add(lblDiscount, gbc_lblDiscount);
-		
-		lblDiscountValue = new JLabel("N/A");
-		lblDiscountValue.setForeground(new Color(0, 102, 0));
-		GridBagConstraints gbc_lblDiscountValue = new GridBagConstraints();
-		gbc_lblDiscountValue.anchor = GridBagConstraints.WEST;
-		gbc_lblDiscountValue.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDiscountValue.gridx = 2;
-		gbc_lblDiscountValue.gridy = 1;
-		priceAndSubmitPanel.add(lblDiscountValue, gbc_lblDiscountValue);
-		
-		JLabel lblTotal = new JLabel("Total:");
-		GridBagConstraints gbc_lblTotal = new GridBagConstraints();
-		gbc_lblTotal.insets = new Insets(0, 0, 5, 5);
-		gbc_lblTotal.anchor = GridBagConstraints.WEST;
-		gbc_lblTotal.gridx = 0;
-		gbc_lblTotal.gridy = 2;
-		priceAndSubmitPanel.add(lblTotal, gbc_lblTotal);
-		
-		lblTotalValue = new JLabel("N/A");
-		GridBagConstraints gbc_lblTotalValue = new GridBagConstraints();
-		gbc_lblTotalValue.anchor = GridBagConstraints.WEST;
-		gbc_lblTotalValue.insets = new Insets(0, 0, 5, 5);
-		gbc_lblTotalValue.gridx = 2;
-		gbc_lblTotalValue.gridy = 2;
-		priceAndSubmitPanel.add(lblTotalValue, gbc_lblTotalValue);
-		
-		JButton btnCreateQuote = new JButton("Create quote");
-		GridBagConstraints gbc_btnCreateQuote = new GridBagConstraints();
-		gbc_btnCreateQuote.anchor = GridBagConstraints.EAST;
-		gbc_btnCreateQuote.gridx = 3;
-		gbc_btnCreateQuote.gridy = 3;
-		priceAndSubmitPanel.add(btnCreateQuote, gbc_btnCreateQuote);
+				// ***** Subtotal label *****
+				JLabel lblSubtotal = new JLabel("Subtotal:");
+				GridBagConstraints gbc_lblSubtotal = new GridBagConstraints();
+				gbc_lblSubtotal.anchor = GridBagConstraints.WEST;
+				gbc_lblSubtotal.insets = new Insets(0, 0, 5, 5);
+				gbc_lblSubtotal.gridx = 0;
+				gbc_lblSubtotal.gridy = 0;
+				priceAndSubmitPanel.add(lblSubtotal, gbc_lblSubtotal);
+				
+				// ***** Subtotal value *****
+				lblSubtotalValue = new JLabel("N/A");
+				lblSubtotalValue.setForeground(new Color(102, 102, 102));
+				GridBagConstraints gbc_lblSubtotalValue = new GridBagConstraints();
+				gbc_lblSubtotalValue.anchor = GridBagConstraints.WEST;
+				gbc_lblSubtotalValue.insets = new Insets(0, 0, 5, 5);
+				gbc_lblSubtotalValue.gridx = 2;
+				gbc_lblSubtotalValue.gridy = 0;
+				priceAndSubmitPanel.add(lblSubtotalValue, gbc_lblSubtotalValue);
+				
+				// ***** Customer type discount label *****
+				JLabel lblDiscount = new JLabel(customer.getCustomerType().getName() + " Discount:");
+				GridBagConstraints gbc_lblDiscount = new GridBagConstraints();
+				gbc_lblDiscount.anchor = GridBagConstraints.WEST;
+				gbc_lblDiscount.insets = new Insets(0, 0, 5, 5);
+				gbc_lblDiscount.gridx = 0;
+				gbc_lblDiscount.gridy = 1;
+				priceAndSubmitPanel.add(lblDiscount, gbc_lblDiscount);
+				
+				// ***** Customer type discount value *****
+				lblDiscountValue = new JLabel("N/A");
+				lblDiscountValue.setForeground(new Color(0, 102, 0));
+				GridBagConstraints gbc_lblDiscountValue = new GridBagConstraints();
+				gbc_lblDiscountValue.anchor = GridBagConstraints.WEST;
+				gbc_lblDiscountValue.insets = new Insets(0, 0, 5, 5);
+				gbc_lblDiscountValue.gridx = 2;
+				gbc_lblDiscountValue.gridy = 1;
+				priceAndSubmitPanel.add(lblDiscountValue, gbc_lblDiscountValue);
+				
+				// ***** Total price label *****
+				JLabel lblTotal = new JLabel("Total:");
+				GridBagConstraints gbc_lblTotal = new GridBagConstraints();
+				gbc_lblTotal.insets = new Insets(0, 0, 5, 5);
+				gbc_lblTotal.anchor = GridBagConstraints.WEST;
+				gbc_lblTotal.gridx = 0;
+				gbc_lblTotal.gridy = 2;
+				priceAndSubmitPanel.add(lblTotal, gbc_lblTotal);
+				
+				// ***** Total price value *****
+				lblTotalValue = new JLabel("N/A");
+				GridBagConstraints gbc_lblTotalValue = new GridBagConstraints();
+				gbc_lblTotalValue.anchor = GridBagConstraints.WEST;
+				gbc_lblTotalValue.insets = new Insets(0, 0, 5, 5);
+				gbc_lblTotalValue.gridx = 2;
+				gbc_lblTotalValue.gridy = 2;
+				priceAndSubmitPanel.add(lblTotalValue, gbc_lblTotalValue);
+				
+				// ***** Create quote button *****
+				JButton btnCreateQuote = new JButton("Create quote");
+				GridBagConstraints gbc_btnCreateQuote = new GridBagConstraints();
+				gbc_btnCreateQuote.anchor = GridBagConstraints.EAST;
+				gbc_btnCreateQuote.gridx = 3;
+				gbc_btnCreateQuote.gridy = 3;
+				priceAndSubmitPanel.add(btnCreateQuote, gbc_btnCreateQuote);
 		
 		// Set price
 		this.refreshPriceSection();
@@ -230,6 +248,16 @@ public class ShoppingCart extends JDialog {
 		this.addEventHandlers();
 	}
 	
+	/*
+	 * *******************************************************
+	 * *******************  Methods *******************
+	 * *******************************************************
+	 */
+	
+	/**
+	 * recalculate the subtotal, total price, customer type discount percentage
+	 * and update view
+	 */
 	public void refreshPriceSection() {
 		lblSubtotalValue.setText(String.format("%.2f DKK", 
 				customer.getShoppingCart().calculateSubtotal()));
@@ -239,6 +267,12 @@ public class ShoppingCart extends JDialog {
 				customer.getShoppingCart().calculateTotal()));
 	}
 	
+	
+	/*
+	 * *******************************************************
+	 * *******************  EVENT HANDLERS *******************
+	 * *******************************************************
+	 */
 	public void addEventHandlers() {
 		// Refresh price on table change
 		mainTable.getModel().addTableModelListener(e -> {
@@ -269,5 +303,4 @@ public class ShoppingCart extends JDialog {
 			tableModel.remove(mainTable.getSelectedRow());
 		});
 	}
-	
 }
