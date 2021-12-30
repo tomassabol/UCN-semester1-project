46acb833019419546140e514a8356f1332c10712
HiImDanix <danixkanepe@gmail.com>
2021-12-30 15:50:15 +0100

    Added info message dialog

--- src/gui/Messages.java
+++ src/gui/Messages.java
@@ -11,16 +11,18 @@ public class Messages {
 	}
 	
 	/**
-	 * Show an error message
+	 * Show an error message window.
 	 *
+	 * @param component component component the component to center the window in. It can be null!
 	 * @param message the message
+	 * @param title the title
 	 */
-	public static void Error(String message, String title) {
-        JOptionPane.showMessageDialog(new JFrame(), message, title,
+	public static void error(Component component, String message, String title) {
+        JOptionPane.showMessageDialog(component, message, title,
                 JOptionPane.ERROR_MESSAGE);
 	}
-	public static void show(String message) {
-        Error(message, "Error");
+	public static void error(Component component, String message) {
+		Messages.error(component, message, "Error");
 	}
 	
 	/**
@@ -31,14 +33,28 @@ public class Messages {
 	 * @param title the title
 	 * @return true, if successful
 	 */
-	public static boolean confirmation(Component component, String message, String title) {
+	public static boolean confirm(Component component, String message, String title) {
 		int confirmResult = JOptionPane.showConfirmDialog(component, message, title, JOptionPane.YES_NO_OPTION);
         return confirmResult == 0 ? true : false;
 	}
-	public static boolean confirmation(Component component, String message) {
-		return confirmation(null, message, "Confirm");
+	public static boolean confirm(Component component, String message) {
+		return Messages.confirm(null, message, "Confirm");
 	}
 	
+	/**
+	 * Show an information window
+	 *
+	 * @param component component the component to center the window in. It can be null!
+	 * @param message the message
+	 * @param title the title
+	 */
+	public static void info(Component component, String message, String title) {
+		JOptionPane.showMessageDialog(component, message, title, JOptionPane.INFORMATION_MESSAGE);
+		
+	}
+	public static void info(Component component, String message) {
+		Messages.info(component, message, "Information");
+	}
 	
 	
 
