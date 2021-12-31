720570f713df48a21402c179860b11b83d2265a4
HiImDanix <danixkanepe@gmail.com>
2022-01-01 00:40:52 +0100

    UI change to show itemLines for chosen quote

--- src/gui/QuotesUI.java
+++ src/gui/QuotesUI.java
@@ -43,14 +43,11 @@ public class QuotesUI extends JDialog {
 	private JPanel contentPane;
 	private JTable table;
 	private QuotesTableModel tableModel;
-	private JTable mainTable;
-	private JButton btnViewInDetail;
+	private JTable quotesTable;
 	
 	QuoteController quoteCtrl;
-	private JLabel lblDiscountValue;
-	private JLabel lblTotalValue;
-	private JLabel lblSubtotalValue;
 	private JTextField txtSearch;
+	private JTable table_1;
 
 	/**
 	 * Create the dialog.
@@ -123,110 +120,47 @@ public class QuotesUI extends JDialog {
 		
 				// ***** Table *****
 				tableModel = new QuotesTableModel(quoteCtrl.getQuotes(customer));
-				mainTable = new JTable();
-				mainTable.setModel(tableModel);
-				mainTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-				scrollPanel.setViewportView(mainTable);
+				quotesTable = new JTable();
+				quotesTable.setModel(tableModel);
+				quotesTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				scrollPanel.setViewportView(quotesTable);
 		
 		// ***** Bottom panel *****
 		JPanel bottomPanel = new JPanel();
 		contentPane.add(bottomPanel, BorderLayout.SOUTH);
-		bottomPanel.setLayout(new BorderLayout(0, 0));
-		
-			// ***** Panel for table options at the bottom *****
-			JPanel tableBottomOptionsPanel = new JPanel();
-			bottomPanel.add(tableBottomOptionsPanel, BorderLayout.NORTH);
-			GridBagLayout gbl_tableBottomOptionsPanel = new GridBagLayout();
-			gbl_tableBottomOptionsPanel.columnWidths = new int[]{0, 0, 0};
-			gbl_tableBottomOptionsPanel.rowHeights = new int[]{0, 0};
-			gbl_tableBottomOptionsPanel.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-			gbl_tableBottomOptionsPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-			tableBottomOptionsPanel.setLayout(gbl_tableBottomOptionsPanel);
-				
-						// ***** Button: View *****
-						btnViewInDetail = new JLink("View", JLink.COLORS.GREEN);
-						btnViewInDetail.setEnabled(false);
-						GridBagConstraints gbc_btnViewInDetail = new GridBagConstraints();
-						gbc_btnViewInDetail.gridx = 1;
-						gbc_btnViewInDetail.gridy = 0;
-						tableBottomOptionsPanel.add(btnViewInDetail, gbc_btnViewInDetail);
-		
-			// ***** Panel for pricing and create quote button *****
-			JPanel priceAndPayPanel = new JPanel();
-			bottomPanel.add(priceAndPayPanel, BorderLayout.SOUTH);
-			GridBagLayout gbl_priceAndPayPanel = new GridBagLayout();
-			gbl_priceAndPayPanel.columnWidths = new int[]{0, 17, 0, 0, 0};
-			gbl_priceAndPayPanel.rowHeights = new int[]{0, 0, 0, 0, 0};
-			gbl_priceAndPayPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-			gbl_priceAndPayPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-			priceAndPayPanel.setLayout(gbl_priceAndPayPanel);
-		
-				// ***** Subtotal label *****
-				JLabel lblSubtotal = new JLabel("Subtotal:");
-				GridBagConstraints gbc_lblSubtotal = new GridBagConstraints();
-				gbc_lblSubtotal.anchor = GridBagConstraints.WEST;
-				gbc_lblSubtotal.insets = new Insets(0, 0, 5, 5);
-				gbc_lblSubtotal.gridx = 0;
-				gbc_lblSubtotal.gridy = 0;
-				priceAndPayPanel.add(lblSubtotal, gbc_lblSubtotal);
-				
-				// ***** Subtotal value *****
-				lblSubtotalValue = new JLabel("N/A");
-				lblSubtotalValue.setForeground(new Color(102, 102, 102));
-				GridBagConstraints gbc_lblSubtotalValue = new GridBagConstraints();
-				gbc_lblSubtotalValue.anchor = GridBagConstraints.WEST;
-				gbc_lblSubtotalValue.insets = new Insets(0, 0, 5, 5);
-				gbc_lblSubtotalValue.gridx = 2;
-				gbc_lblSubtotalValue.gridy = 0;
-				priceAndPayPanel.add(lblSubtotalValue, gbc_lblSubtotalValue);
-				
-				// ***** Customer type discount label *****
-				JLabel lblDiscount = new JLabel(customer.getCustomerType().getName() + " Discount:");
-				GridBagConstraints gbc_lblDiscount = new GridBagConstraints();
-				gbc_lblDiscount.anchor = GridBagConstraints.WEST;
-				gbc_lblDiscount.insets = new Insets(0, 0, 5, 5);
-				gbc_lblDiscount.gridx = 0;
-				gbc_lblDiscount.gridy = 1;
-				priceAndPayPanel.add(lblDiscount, gbc_lblDiscount);
-				
-				// ***** Customer type discount value *****
-				lblDiscountValue = new JLabel("N/A");
-				lblDiscountValue.setForeground(new Color(0, 102, 0));
-				GridBagConstraints gbc_lblDiscountValue = new GridBagConstraints();
-				gbc_lblDiscountValue.anchor = GridBagConstraints.WEST;
-				gbc_lblDiscountValue.insets = new Insets(0, 0, 5, 5);
-				gbc_lblDiscountValue.gridx = 2;
-				gbc_lblDiscountValue.gridy = 1;
-				priceAndPayPanel.add(lblDiscountValue, gbc_lblDiscountValue);
-				
-				// ***** Total price label *****
-				JLabel lblTotal = new JLabel("Total:");
-				GridBagConstraints gbc_lblTotal = new GridBagConstraints();
-				gbc_lblTotal.insets = new Insets(0, 0, 5, 5);
-				gbc_lblTotal.anchor = GridBagConstraints.WEST;
-				gbc_lblTotal.gridx = 0;
-				gbc_lblTotal.gridy = 2;
-				priceAndPayPanel.add(lblTotal, gbc_lblTotal);
-				
-				// ***** Total price value *****
-				lblTotalValue = new JLabel("N/A");
-				GridBagConstraints gbc_lblTotalValue = new GridBagConstraints();
-				gbc_lblTotalValue.anchor = GridBagConstraints.WEST;
-				gbc_lblTotalValue.insets = new Insets(0, 0, 5, 5);
-				gbc_lblTotalValue.gridx = 2;
-				gbc_lblTotalValue.gridy = 2;
-				priceAndPayPanel.add(lblTotalValue, gbc_lblTotalValue);
-				
-				// ***** Create quote button *****
-				JButton btnPay = new JButton("Pay");
-				GridBagConstraints gbc_btnPay = new GridBagConstraints();
-				gbc_btnPay.anchor = GridBagConstraints.EAST;
-				gbc_btnPay.gridx = 3;
-				gbc_btnPay.gridy = 3;
-				priceAndPayPanel.add(btnPay, gbc_btnPay);
-		
-		// Set price
-		this.setPricing(null);
+					GridBagLayout gbl_bottomPanel = new GridBagLayout();
+					gbl_bottomPanel.columnWidths = new int[]{430, 0, 0};
+					gbl_bottomPanel.rowHeights = new int[]{0, 29, 29, 0, 0};
+					gbl_bottomPanel.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+					gbl_bottomPanel.rowWeights = new double[]{0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+					bottomPanel.setLayout(gbl_bottomPanel);
+					
+					JLabel lblNewLabel = new JLabel("New label");
+					GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+					gbc_lblNewLabel.gridwidth = 2;
+					gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+					gbc_lblNewLabel.gridx = 0;
+					gbc_lblNewLabel.gridy = 0;
+					bottomPanel.add(lblNewLabel, gbc_lblNewLabel);
+					
+					JScrollPane scrollPane = new JScrollPane();
+					GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+					gbc_scrollPane.gridwidth = 2;
+					gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+					gbc_scrollPane.fill = GridBagConstraints.BOTH;
+					gbc_scrollPane.gridx = 0;
+					gbc_scrollPane.gridy = 1;
+					bottomPanel.add(scrollPane, gbc_scrollPane);
+					
+					table_1 = new JTable();
+					scrollPane.setRowHeaderView(table_1);
+					
+					JButton btnPay = new JButton("Pay");
+					GridBagConstraints gbc_btnPay = new GridBagConstraints();
+					gbc_btnPay.insets = new Insets(0, 0, 5, 0);
+					gbc_btnPay.gridx = 1;
+					gbc_btnPay.gridy = 2;
+					bottomPanel.add(btnPay, gbc_btnPay);
 		
 		// Attach event handler
 		this.addEventHandlers();
@@ -238,27 +172,7 @@ public class QuotesUI extends JDialog {
 	 * *******************************************************
 	 */
 	
-	/**
-	 * Calculate the subtotal, total price, customer type discount percentage
-	 * and update view
-	 */
-	// TODO: REFRESH PRICING DEPENDING ON WHICH QUOTE IS SELECTED
-	public void setPricing(Quote quote) {
-		if (quote == null) {
-			lblSubtotalValue.setText("");
-			lblDiscountValue.setText("");
-			lblTotalValue.setText("");
-		} else {
-			lblSubtotalValue.setText(String.format("%.2f DKK", 
-					quote.getSubtotal()));
-			lblDiscountValue.setText(String.format("%d%%",
-					quote.getCUSTOMER_TYPE_DISCOUNT_PERCENTAGE()));
-			lblTotalValue.setText(String.format("%.2f DKK", 
-					quote.getTotalPrice()));
-		}
 
-	}
-	
 	
 	/*
 	 * *******************************************************
@@ -268,17 +182,11 @@ public class QuotesUI extends JDialog {
 	public void addEventHandlers() {
 		
 		// When a row is selected/unselected:
-		// show price for the selected quote
-		// toggle table's bottom options
-		mainTable.getSelectionModel().addListSelectionListener(e -> {
+		quotesTable.getSelectionModel().addListSelectionListener(e -> {
 			// not selected
-			if (mainTable.getSelectionModel().isSelectionEmpty()) {
-				btnViewInDetail.setEnabled(false);
-				setPricing(null);
+			if (quotesTable.getSelectionModel().isSelectionEmpty()) {
 			// selected
 			} else {
-				btnViewInDetail.setEnabled(true);
-				setPricing(tableModel.getQuote(mainTable.getSelectedRow()));
 			}
 		});
 		
