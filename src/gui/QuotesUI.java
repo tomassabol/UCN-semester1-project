4ced93bc59404f0ecb19dcd528a93357ad500d08
HiImDanix <danixkanepe@gmail.com>
2022-01-04 19:12:24 +0100

    UI refactor to make item lines table grow as well

--- src/gui/QuotesUI.java
+++ src/gui/QuotesUI.java
@@ -46,13 +46,13 @@ public class QuotesUI extends JDialog {
 	private JTable table;
 	private QuotesTableModel quotesTableModel;
 	private QuotesItemTableModel itemTableModel;
-	private JTable tableQuotes;
 	
 	QuoteController quoteCtrl;
 	private JTextField txtSearch;
-	private JTable tableItems;
 	private JButton btnCreateQuote;
 	private JButton btnPay;
+	private JTable tableQuotes;
+	private JTable tableItems;
 	
 
 	/**
@@ -74,101 +74,100 @@ public class QuotesUI extends JDialog {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{451, 0};
+		gbl_contentPane.rowHeights = new int[]{50, 114, 0, 49, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
 		
 		// ***** TOP PANEL *****
 		JPanel topPanel = new JPanel();
-		getContentPane().add(topPanel, BorderLayout.NORTH);
+		GridBagConstraints gbc_topPanel = new GridBagConstraints();
+		gbc_topPanel.anchor = GridBagConstraints.NORTH;
+		gbc_topPanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_topPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_topPanel.gridx = 0;
+		gbc_topPanel.gridy = 0;
+		getContentPane().add(topPanel, gbc_topPanel);
 		GridBagLayout gbl_topPanel = new GridBagLayout();
 		gbl_topPanel.columnWidths = new int[]{0, 0, 0, 0};
 		gbl_topPanel.rowHeights = new int[]{0, 0, 0, 0};
 		gbl_topPanel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
 		gbl_topPanel.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
 		topPanel.setLayout(gbl_topPanel);
-			// ***** Title *****
-			JLabel lblTitle = new JLabel(
-					String.format("%s %s's quotes", 
-							customer.getFirstName(), 
-							customer.getLastName()
-					)
-			);
-			GridBagConstraints gbc_lblTitle = new GridBagConstraints();
-			gbc_lblTitle.gridwidth = 3;
-			gbc_lblTitle.insets = new Insets(0, 0, 5, 5);
-			gbc_lblTitle.gridx = 0;
-			gbc_lblTitle.gridy = 0;
-			topPanel.add(lblTitle, gbc_lblTitle);
-			
-			txtSearch = new JTextField();
-			txtSearch.setToolTipText("Search");
-			GridBagConstraints gbc_txtSearch = new GridBagConstraints();
-			gbc_txtSearch.insets = new Insets(0, 0, 5, 5);
-			gbc_txtSearch.fill = GridBagConstraints.HORIZONTAL;
-			gbc_txtSearch.gridx = 0;
-			gbc_txtSearch.gridy = 1;
-			topPanel.add(txtSearch, gbc_txtSearch);
-			txtSearch.setColumns(10);
-			
-			btnCreateQuote = new JButton("Create quote");
-			GridBagConstraints gbc_btnCreateQuote = new GridBagConstraints();
-			gbc_btnCreateQuote.insets = new Insets(0, 0, 5, 0);
-			gbc_btnCreateQuote.gridx = 2;
-			gbc_btnCreateQuote.gridy = 1;
-			topPanel.add(btnCreateQuote, gbc_btnCreateQuote);
-			
-		// ***** Middle panel *****
-		JPanel middlePanel = new JPanel();
-		getContentPane().add(middlePanel, BorderLayout.CENTER);
-		middlePanel.setLayout(new BorderLayout(0, 0));
-			
-			// ***** Scroll panel *****
-			JScrollPane scrollPanel = new JScrollPane();
-			middlePanel.add(scrollPanel);
+		// ***** Title *****
+		JLabel lblTitle = new JLabel(
+				String.format("%s %s's quotes", 
+						customer.getFirstName(), 
+						customer.getLastName()
+				)
+		);
+		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+		gbc_lblTitle.gridwidth = 3;
+		gbc_lblTitle.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTitle.gridx = 0;
+		gbc_lblTitle.gridy = 0;
+		topPanel.add(lblTitle, gbc_lblTitle);
 		
-				// ***** Table *****
-				quotesTableModel = new QuotesTableModel(quoteCtrl.getQuotes(customer));
-				tableQuotes = new JTable();
-				tableQuotes.setModel(quotesTableModel);
-				tableQuotes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-				scrollPanel.setViewportView(tableQuotes);
+		txtSearch = new JTextField();
+		txtSearch.setToolTipText("Search");
+		GridBagConstraints gbc_txtSearch = new GridBagConstraints();
+		gbc_txtSearch.insets = new Insets(0, 0, 5, 5);
+		gbc_txtSearch.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtSearch.gridx = 0;
+		gbc_txtSearch.gridy = 1;
+		topPanel.add(txtSearch, gbc_txtSearch);
+		txtSearch.setColumns(10);
 		
-		// ***** Bottom panel *****
-		JPanel bottomPanel = new JPanel();
-		contentPane.add(bottomPanel, BorderLayout.SOUTH);
-					GridBagLayout gbl_bottomPanel = new GridBagLayout();
-					gbl_bottomPanel.columnWidths = new int[]{430, 0, 0};
-					gbl_bottomPanel.rowHeights = new int[]{0, 29, 29, 0, 0};
-					gbl_bottomPanel.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-					gbl_bottomPanel.rowWeights = new double[]{0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-					bottomPanel.setLayout(gbl_bottomPanel);
-					
-					JLabel lblItemsInQuote = new JLabel("Items in quote");
-					GridBagConstraints gbc_lblItemsInQuote = new GridBagConstraints();
-					gbc_lblItemsInQuote.gridwidth = 2;
-					gbc_lblItemsInQuote.insets = new Insets(0, 0, 5, 5);
-					gbc_lblItemsInQuote.gridx = 0;
-					gbc_lblItemsInQuote.gridy = 0;
-					bottomPanel.add(lblItemsInQuote, gbc_lblItemsInQuote);
-					
-					JScrollPane scrollPane = new JScrollPane();
-					GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-					gbc_scrollPane.gridwidth = 2;
-					gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
-					gbc_scrollPane.fill = GridBagConstraints.BOTH;
-					gbc_scrollPane.gridx = 0;
-					gbc_scrollPane.gridy = 1;
-					bottomPanel.add(scrollPane, gbc_scrollPane);
-					
-					tableItems = new JTable();
-					scrollPane.setRowHeaderView(tableItems);
-					
-					btnPay = new JButton("Pay");
-					btnPay.setEnabled(false);
-					GridBagConstraints gbc_btnPay = new GridBagConstraints();
-					gbc_btnPay.insets = new Insets(0, 0, 5, 0);
-					gbc_btnPay.gridx = 1;
-					gbc_btnPay.gridy = 2;
-					bottomPanel.add(btnPay, gbc_btnPay);
+		btnCreateQuote = new JButton("Create quote");
+		GridBagConstraints gbc_btnCreateQuote = new GridBagConstraints();
+		gbc_btnCreateQuote.insets = new Insets(0, 0, 5, 0);
+		gbc_btnCreateQuote.gridx = 2;
+		gbc_btnCreateQuote.gridy = 1;
+		topPanel.add(btnCreateQuote, gbc_btnCreateQuote);
+		
+		
+		JScrollPane scrollPanel = new JScrollPane();
+		GridBagConstraints gbc_scrollPanel = new GridBagConstraints();
+		gbc_scrollPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPanel.fill = GridBagConstraints.BOTH;
+		gbc_scrollPanel.gridx = 0;
+		gbc_scrollPanel.gridy = 1;
+		contentPane.add(scrollPanel, gbc_scrollPanel);
+		
+		tableQuotes = new JTable();
+		quotesTableModel = new QuotesTableModel(quoteCtrl.getQuotes(customer));
+		tableQuotes.setModel(quotesTableModel);
+		scrollPanel.setViewportView(tableQuotes);
+		
+		JLabel lblItemsInQuote = new JLabel("Items in quote");
+		GridBagConstraints gbc_lblItemsInQuote = new GridBagConstraints();
+		gbc_lblItemsInQuote.insets = new Insets(0, 0, 5, 0);
+		gbc_lblItemsInQuote.gridx = 0;
+		gbc_lblItemsInQuote.gridy = 2;
+		contentPane.add(lblItemsInQuote, gbc_lblItemsInQuote);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 3;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		tableItems = new JTable();
+		scrollPane.setViewportView(tableItems);
+		
+		btnPay = new JButton("Pay");
+		btnPay.setEnabled(false);
+		GridBagConstraints gbc_btnPay = new GridBagConstraints();
+		gbc_btnPay.anchor = GridBagConstraints.EAST;
+		gbc_btnPay.gridx = 0;
+		gbc_btnPay.gridy = 4;
+		contentPane.add(btnPay, gbc_btnPay);
+		
+		
 		
 		// Attach event handler
 		this.addEventHandlers();
@@ -197,31 +196,28 @@ public class QuotesUI extends JDialog {
 			}
 		});
 		
-		// When a row is selected/unselected:
+		// If a quote is selected
 		tableQuotes.getSelectionModel().addListSelectionListener(e -> {
-			// ***** Not selected *****
 			if (tableQuotes.getSelectionModel().isSelectionEmpty()) {
+				// ***** NOT SELECTED *****
+				
 				// Disable pay button
 				btnPay.setEnabled(false);
-				
-				
-				// Reset item line table model
-				tableItems.setModel(new DefaultTableModel());
 			} else {
-				// ***** Selected *****
+				// ***** SELECTED *****
 				
 				// Enable pay button
 				btnPay.setEnabled(true);
 				
-				// Get selected quote, and show the item lines (by using ShoppingCartTableModel)
-				Quote quote = quotesTableModel.getQuote(tableQuotes.getSelectedRow());
+				// Show the item lines
+				int selectedRow = tableQuotes.getSelectedRow();
+				Quote quote = quotesTableModel.getQuote(selectedRow);
 				itemTableModel = new QuotesItemTableModel(quote.getItemLines());
-				tableItems.setModel(itemTableModel);
-				tableItems.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-				//scrollPanel.setViewportView(mainTable);
-				
+				tableItems.setModel(itemTableModel);;
 			}
 		});
+		
+		
 		
 	}
 }
