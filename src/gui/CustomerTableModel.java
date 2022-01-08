952cd9599c6fbc133c389d6e148d2e671981385a
HiImDanix <danixkanepe@gmail.com>
2022-01-08 22:12:31 +0100

    Implemented customer table model

---
+++ src/gui/CustomerTableModel.java
@@ -0,0 +1,123 @@
+package gui;
+
+
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import controller.CustomerController;
+import controller.QuoteController;
+import model.Customer;
+import model.Quote;
+
+/**
+ * @author Daniels Kanepe
+ *
+ */
+public class CustomerTableModel extends AbstractTableModel {
+	
+	private final static String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+	private static final long serialVersionUID = -2367962812947993282L;
+
+	protected static final String COLUMN_NAMES[] = {
+        "ID", "First name", "Last name", "Address", "Mobile", "Type", "Birth"
+    };
+
+    private List<Customer> customers;
+    private CustomerController customerCtrl;
+
+    
+    /**
+     * Instantiates a new customer table model.
+     *
+     * @param customers the customers
+     */
+    public CustomerTableModel(List<Customer> customers) {
+    	this.customerCtrl = new CustomerController();
+        // Prevent possible mutation
+        this.customers = new ArrayList<Customer>(customers);
+    }
+    
+
+    @Override
+    public int getRowCount() {
+        return customers.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLUMN_NAMES.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return COLUMN_NAMES[column];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            default: return String.class;
+        }
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+    	Customer customer = customers.get(rowIndex);
+        switch (columnIndex) {
+            case 0: return "#" + customer.ID;
+            case 1: return customer.getFirstName();
+            case 2: return customer.getLastName();
+            case 3: return customer.getAddress();
+            case 4: return customer.getMobile();
+            case 5: return customer.getCustomerType();
+            case 6: return customer.getBirthDate().format(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+            default: return "Error retrieving column name";
+        }
+    }
+    
+    // Make cells uneditable
+    @Override
+    public boolean isCellEditable(int row, int column) {       
+        return false;
+    }
+    
+    /**
+     * Gets the customer object by row
+     *
+     * @param row the row
+     * @return the customer
+     */
+    public Customer getCustomer(int row) {
+    	return customers.get(row);
+    }
+    
+  
+    /**
+     * Adds a customer to the table
+     *
+     * @param customer the customer
+     * @return the int
+     */
+    public int addCustomer(Customer customer) {
+    	int row = customers.size();
+        this.customers.add(customer);
+        fireTableRowsInserted(row, row);
+        return row;
+    }
+    
+    /**
+     * Removes the customer from the table by row
+     *
+     * @param row the row
+     */
+    public void removeCustomer(int row) {
+    	this.customers.remove(row);
+    	this.fireTableRowsDeleted(row, row);
+    }
+
+}
