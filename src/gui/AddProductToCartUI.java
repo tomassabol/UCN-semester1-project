32deb3191e10dfd2777ba7a9845deb4181c1b483
HiImDanix <danixkanepe@gmail.com>
2022-01-08 19:02:17 +0100

    Add item: UI mockup

--- src/gui/AddProductToCartUI.java
+++ src/gui/AddProductToCartUI.java
@@ -14,6 +14,10 @@ import javax.swing.border.EmptyBorder;
 import gui.CRUDProductsPanel.Display;
 
 import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JSpinner;
 
 /**
  * @author Daniels Kanepe
@@ -38,10 +42,43 @@ public class AddProductToCartUI extends JDialog {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout());
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 420, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{225, 25, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
 		
 		CRUDProductsPanel panel = new CRUDProductsPanel(Display.BUYABLE);
-		getContentPane().add(panel, BorderLayout.CENTER);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridwidth = 3;
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		getContentPane().add(panel, gbc_panel);
+		
+		JLabel lblQuantity = new JLabel("Quantity");
+		GridBagConstraints gbc_lblQuantity = new GridBagConstraints();
+		gbc_lblQuantity.anchor = GridBagConstraints.EAST;
+		gbc_lblQuantity.insets = new Insets(0, 0, 0, 5);
+		gbc_lblQuantity.gridx = 0;
+		gbc_lblQuantity.gridy = 1;
+		contentPane.add(lblQuantity, gbc_lblQuantity);
+		
+		JSpinner spinner = new JSpinner();
+		GridBagConstraints gbc_spinner = new GridBagConstraints();
+		gbc_spinner.anchor = GridBagConstraints.WEST;
+		gbc_spinner.insets = new Insets(0, 0, 0, 5);
+		gbc_spinner.gridx = 1;
+		gbc_spinner.gridy = 1;
+		contentPane.add(spinner, gbc_spinner);
+		
+		JButton btnChoose = new JButton("Choose");
+		GridBagConstraints gbc_btnChoose = new GridBagConstraints();
+		gbc_btnChoose.gridx = 2;
+		gbc_btnChoose.gridy = 1;
+		contentPane.add(btnChoose, gbc_btnChoose);
 		
 		// Attach event handlers
 		this.addEventHandlers();
