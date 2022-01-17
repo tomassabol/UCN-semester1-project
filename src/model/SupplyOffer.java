1e6001b7971768fdda6b92a021d6230f629f11e6
HiImDanix <danixkanepe@gmail.com>
2022-01-17 17:15:38 +0100

    Added comments

--- src/model/SupplyOffer.java
+++ src/model/SupplyOffer.java
@@ -3,25 +3,44 @@ package model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SupplyOffer.
+ */
 public class SupplyOffer {
+    
+    /** The id. */
     public final int ID;
+    
+    /** The product. */
     public Product product;
-    public  BigDecimal pricePerItem;
+    
+    /** The price per item. */
+    public BigDecimal pricePerItem;
+    
+    /** The min quantity. */
     public int minQuantity;
+    
+    /** The date added. */
     public final LocalDateTime DATE_ADDED;
+    
+    /** The contractor. */
     public Contractor contractor;
+    
+    /** The active. */
     private boolean active;
+
 
 
     /**
      * Instantiates a new supply offer.
      *
      * @param Id the id
-     * @param pricePerItem the price per product
-     * @param minQuantity the minimum order quantity
-     * @param contractor The contractor supplying this offer
-     * @param active whether the offer is active
-     * @param dateAdded the date the offer was created
+     * @param pricePerItem the price per item
+     * @param minQuantity the min quantity
+     * @param contractor the contractor
+     * @param active the active
+     * @param dateAdded the date added
      */
     public SupplyOffer(int Id, BigDecimal pricePerItem,
     		int minQuantity, Contractor contractor,
@@ -35,50 +54,100 @@ public class SupplyOffer {
     }
 
 
+	/**
+	 * Checks if is active.
+	 *
+	 * @return true, if is active
+	 */
 	public boolean isActive() {
 		return active;
 	}
 
 
+	/**
+	 * Sets the active.
+	 *
+	 * @param active the new active
+	 */
 	public void setActive(boolean active) {
 		this.active = active;
 	}
 
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public int getID() {
 		return ID;
 	}
 
+	/**
+	 * Gets the date added.
+	 *
+	 * @return the date added
+	 */
 	public LocalDateTime getDateAdded() {
 		return DATE_ADDED;
 	}
 
 
+	/**
+	 * Gets the price per item.
+	 *
+	 * @return the price per item
+	 */
 	public BigDecimal getPricePerItem() {
 		return pricePerItem;
 	}
 
 
+	/**
+	 * Sets the price per item.
+	 *
+	 * @param pricePerItem the new price per item
+	 */
 	public void setPricePerItem(BigDecimal pricePerItem) {
 		this.pricePerItem = pricePerItem;
 	}
 
 
+	/**
+	 * Gets the min quantity.
+	 *
+	 * @return the min quantity
+	 */
 	public int getMinQuantity() {
 		return minQuantity;
 	}
 
 
+	/**
+	 * Sets the min quantity.
+	 *
+	 * @param minQuantity the new min quantity
+	 */
 	public void setMinQuantity(int minQuantity) {
 		this.minQuantity = minQuantity;
 	}
 
 
+	/**
+	 * Gets the contractor.
+	 *
+	 * @return the contractor
+	 */
 	public Contractor getContractor() {
 		return contractor;
 	}
 
 
+	/**
+	 * Sets the contractor.
+	 *
+	 * @param contractor the new contractor
+	 */
 	public void setContractor(Contractor contractor) {
 		this.contractor = contractor;
 	}
