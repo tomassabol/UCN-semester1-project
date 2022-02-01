c5eda44b8dadab8acb9330c97d36c528ec288f5e
unknown <tomassabol2@gmail.com>
2022-02-01 22:00:08 +0100

    generated javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"gui"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"exception"},{"l":"gui"},{"l":"gui.panels"},{"l":"gui.panels.tableModels"},{"l":"gui.statistics.charts"},{"l":"gui.windows"},{"l":"gui.windows.model"},{"l":"models"},{"l":"models.containers"},{"l":"tests"},{"l":"view"}];updateSearchResults();
